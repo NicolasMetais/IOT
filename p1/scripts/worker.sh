6397edf8d98d4ca4ac9ab07e2f280beceae0ae1c
NicolasMetais <nico.metais@hotmail.fr>
2026-02-21 10:35:40 +0100

    yes

--- p1/scripts/worker.sh
+++ p1/scripts/worker.sh
@@ -3,6 +3,6 @@ apt upgrade -y
 
 apt install -y curl
 
-TOKEN=$(cat /vagrant/k3s)
+TOKEN=$(cat /vagrant/confs/k3s)
 
 curl -sfL https://get.k3s.io | K3S_URL="https://192.168.56.110:6443" K3S_TOKEN=$TOKEN sh -
