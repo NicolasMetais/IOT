3e579abb21d4541e10d97e6c1cab50fb766074f5
NicolasMetais <nico.metais@hotmail.fr>
2026-03-12 04:57:02 +0100

    it's ok

--- p1/scripts/server.sh
+++ p1/scripts/server.sh
@@ -3,6 +3,6 @@ apt upgrade -y
 
 apt install -y curl
 
-curl -sfL https://get.k3s.io | INSTALL_K3S_EXEC="--bind-address 192.168.56.110 --node-ip 192.168.56.110 --tls-san 192.168.56.110" sh -
+curl -sfL https://get.k3s.io | INSTALL_K3S_EXEC="--bind-address 192.168.56.110 --node-ip 192.168.56.111 --tls-san 192.168.56.110" sh -
 
 sudo cat /var/lib/rancher/k3s/server/node-token >/vagrant/confs/k3s
--- p2/scripts/server.sh
+++ p2/scripts/server.sh
@@ -10,9 +10,4 @@ echo "192.168.56.110 app1.com app2.com app3.com" | sudo tee -a /etc/hosts
 sudo kubectl apply -f /vagrant/confs/app.yaml
 sudo kubectl apply -f /vagrant/confs/ingress.yaml
 
-
-sleep 10
-
-# sudo kubectl cp /vagrant/confs/index1.html default/app1-service:/usr/share/nginx/html/index.html
-# sudo kubectl cp /vagrant/confs/index2.html default/app2-service:/usr/share/nginx/html/index.html
-# sudo kubectl cp /vagrant/confs/index3.html default/app3-service:/usr/share/nginx/html/index.html
+#kubectl get ingress Pour voir le traefik
